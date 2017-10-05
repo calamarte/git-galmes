e9d1290920ede8760308b52576f18be21248aa67
calamarte <calamarte@gmail.com>
2017-10-06 01:33:59 +0200

    a tope con gulp

--- solucio_kittens/gulpfile.js
+++ solucio_kittens/gulpfile.js
@@ -5,23 +5,23 @@ let compilaSCSS = require('gulp-sass');
 let concatJS = require('gulp-concat');
 let concatCSS = require('gulp-concat-css');
 let clean = require('gulp-clean');
+let inject = require('gulp-inject');
+let deleteLines = require('gulp-delete-lines');
 
 gulp.task('minimizarCSS',['compiladorSass','unirCSS'], () => {
-    return gulp.src('css/all.css').pipe(limpiaCSS()).pipe(gulp.dest('dist'));
+    return gulp.src('css/all.css').pipe(limpiaCSS()).pipe(gulp.dest('dist/css'));
 });
 
 gulp.task('minimizarJS',['unirJS'],() =>{
-    return gulp.src('js/all.js').pipe(limpiaJS()).pipe(gulp.dest('dist'));
+    return gulp.src('js/all.js').pipe(limpiaJS()).pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('compiladorSass:watch', function () {
+    gulp.watch('./sass/**/*.scss', ['compiladorSass']);
 });
 
 gulp.task('compiladorSass', () =>{
     return gulp.src('sass/**/*.scss').pipe(compilaSCSS()).pipe(gulp.dest('css'));
-});
-
-gulp.task('watch',()=>{
-   gulp.watch('sass/**/*.scss',()=>{
-      console.log('compilando sass');
-   });
 });
 
 gulp.task('unirJS',()=>{
@@ -34,7 +34,23 @@ gulp.task('unirCSS',()=>{
        .pipe(concatCSS('all.css')).pipe(gulp.dest('css'));
 });
 
-gulp.task('clean', function() {
-    return gulp.src('dist')
-        .pipe(clean());
+gulp.task('clean', ()=> {
+    return gulp.src('dist').pipe(clean());
+});
+
+gulp.task('concatClean',()=>{
+   return gulp.src(['css/all.css',"js/all.js"]).pipe(clean());
+});
+
+gulp.task('injección',['minimizarCSS','minimizarJS','copy'],()=> {
+        let target = gulp.src('dist/index.html');
+        let sources = gulp.src(['dist/js/*.js', 'dist/css/*.css'], {read: false});
+        let filterJs = deleteLines({'filters': [/<script/i]});
+        let filterCss = deleteLines({'filters': [/<link/i]});
+
+        return target.pipe(filterJs).pipe(filterCss).pipe(inject(sources, {relative: true})).pipe(gulp.dest('dist'));
+});
+
+gulp.task('copy',()=>{
+    return gulp.src('index.html').pipe(gulp.dest('dist'));
 });
