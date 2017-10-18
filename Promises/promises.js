01aea01faa0bcbbe44ae70bd7c0910a5bbd35777
calamarte <calamarte@gmail.com>
2017-10-18 11:14:20 +0200

    Ya se construye toda la tabla pero hay problemas con los botones

--- Promises/promises.js
+++ Promises/promises.js
@@ -1,17 +1,19 @@
 function ajax () {
-   return new Promise((resolve, reject) => {
     for (let i = 0; i < 10; i += 1) {
-      let xhttp = new XMLHttpRequest();
-      xhttp.onreadystatechange = function () {
-        if (this.readyState === 4 && this.status === 200) {
-          // console.log(this.responseText);
-        }
-      };
-      xhttp.open("GET", "http://35.194.72.13/asincron.php", true);
-      xhttp.send();
+      let promise = new Promise((resolve, reject) => {
+        let xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function () {
+          if (this.readyState === 4 && this.status === 200) {
+            // console.log(this.responseText);
+          }
+        };
+        xhttp.open("GET", "http://35.194.72.13/asincron.php", true);
+        xhttp.send();
+      });
     }
-  });
+  return promise;
 }
+
 Promise.all([ajax()]).then(()=>{
   alert("listooooo");
 });
--- VetPlus/js/animals.js
+++ VetPlus/js/animals.js
@@ -1,6 +1,6 @@
 let datos = [];
 function createHeader() {
-  const cabeceras = ['Id', 'Nom', 'Numero de registre', 'espècie'];
+  const cabeceras = ['Nom', 'Numero de registre', 'espècie'];
   const fila = document.createElement('tr');
   for (let i = 0; i < cabeceras.length; i += 1) {
     const celda = document.createElement('th');
@@ -10,17 +10,68 @@ function createHeader() {
   return fila;
 }
 
-function createTable(datos) {
+function editB() {
+  console.log('editando');
+}
+
+// Necesito encontrar el objeto y no puedo sino paso un parametro
+// Yo digo que aqui hay que hacer cosas con callback
+
+function deleteB() {
+  console.log('borrando');
+}
+
+function createTable() {
   const tabla = document.createElement('table');
-  tabla.id = 'table';
+  tabla.id = 'tabla';
   tabla.appendChild(createHeader());
+  for (let i = 0; i < datos.length; i += 1) {
+    const fila = document.createElement('tr');
+
+    const nombre = document.createElement('td');
+    const num = document.createElement('td');
+    const especie = document.createElement('td');
+    const botones = document.createElement('td');
+
+    const editar = document.createElement('input');
+    const borrar = document.createElement('input');
+
+    editar.type = 'button';
+    editar.value = 'editar';
+    editar.onclick = editB;
+    borrar.type = 'button';
+    borrar.value = 'borrar';
+    borrar.onclick = deleteB;
+
+    nombre.textContent = datos[i].nomAnimal;
+    if (datos[i].numregistre)num.textContent = datos[i].numregistre;
+    else num.textContent = 'Sense número';
+    especie.textContent = datos[i].nomTipus;
+    botones.appendChild(editar);
+    botones.appendChild(borrar);
+
+    fila.appendChild(nombre);
+    fila.appendChild(num);
+    fila.appendChild(especie);
+    fila.appendChild(botones);
+
+    tabla.appendChild(fila);
+  }
+  document.body.appendChild(tabla);
 }
 
 const xhttp = new XMLHttpRequest();
-xhttp.onreadystatechange = () => {
+xhttp.onreadystatechange = function () {
   if (this.readyState === 4 && this.status === 200) {
     datos = JSON.parse(this.responseText);
+    console.log(datos);
+    createTable();
   }
 };
-xhttp.open('POST', "", true);
-xhttp.send();
+
+xhttp.open('POST', 'http://35.194.72.13/vetplus/serveis.php', true);
+xhttp.send(JSON.stringify({
+  MethodName: 'getAnimals',
+  params: ''
+}));
+
