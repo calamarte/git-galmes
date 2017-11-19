086b35d80704d2774211f19146a2d0699e0988b7
calamarte <calamarte@gmail.com>
2017-11-19 23:38:41 +0100

    lito

--- city-piker/script.js
+++ city-piker/script.js
@@ -1,27 +1,22 @@
-//    w = new Worker('worker.js');
+let w = new Worker('worker.js');
 let data = false;
 let keys = [];
 let allCities = [];
+let mark;
 
-getCities();
 
-async function getCities() {
-  let fetchCities = await fetch('https://raw.githubusercontent.com/David-Haim/CountriesToCitiesJSON/master/countriesToCities.json');
-  data = await fetchCities.json();
+
+w.onmessage = (event)=>{
+  data = event.data;
 
   for (let key in data){
     keys.push(key);
     allCities = allCities.concat(data[key]);
   }
-
-}
-
-//    w.onmessage(function (event){
-//      data = event.data;
-//    });
+};
 
 document.querySelector('#search').addEventListener('change',(e)=>{
-  if(data && e.target.value.length >= 3)search();
+  if(data && e.target.value.length >= 3)onHtml(search());
 });
 
 function search() {
@@ -30,21 +25,22 @@ function search() {
 
 
   const keyFilter = keys.filter((country)=>{
-    return country.toLowerCase().startsWith(document.querySelector('#search').value.toLowerCase());
+    return country.toLowerCase().includes(document.querySelector('#search')
+    .value.toLowerCase());
   });
 
   const citiesFilter = allCities.filter((city)=>{
-    return city.toLowerCase().startsWith(document.querySelector('#search').value.toLowerCase());
+    return city.toLowerCase().includes(document.querySelector('#search')
+    .value.toLowerCase());
   });
 
 
   for (let i = 0; i < keyFilter.length;i++){
-    console.log(keyFilter[i],data[keyFilter[i]]);
     citiesCountry = citiesCountry.concat(data[keyFilter[i]]);
   }
 
   set = new Set(citiesCountry.concat(citiesFilter));
-  onHtml(set.toArray());
+  return set.toArray();
 }
 
 function onHtml(divs) {
@@ -55,12 +51,14 @@ function onHtml(divs) {
   let div = document.createElement('div');
   div.id = 'cities';
 
-
   for(let i = 0; i < divs.length;i++){
     let ciudad = document.createElement('div');
 
     ciudad.textContent = divs[i];
     div.appendChild(ciudad);
   }
+
   document.body.appendChild(div);
+  mark = new Mark(document.querySelector('div'));
+  mark.mark(document.querySelector('#search').value);
 }
--- city-piker/worker.js
+++ city-piker/worker.js
@@ -1,8 +1,7 @@
 async function getCities() {
   let fetchCities = await fetch('https://raw.githubusercontent.com/David-Haim/CountriesToCitiesJSON/master/countriesToCities.json');
   let cities = await fetchCities.json();
-  console.log(cities);
-  // postMessage(cities);
+  postMessage(cities);
 }
 
 getCities();
